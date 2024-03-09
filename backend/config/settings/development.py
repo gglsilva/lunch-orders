6b2fbac41f983285b6c2d9e9034072bd03e43022
gglsilva <gabrielchamex@gmail.com>
2024-03-09 19:07:28 -0300

    add

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -1,6 +1,10 @@
 from config.settings.base import *
+import os
+import dj_database_url
+
 
 DEBUG = True
+
 
 # DATABASES = {
 #     'default': {
@@ -9,15 +13,28 @@ DEBUG = True
 #     }
 # }
 
+# local
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": config("DB_NAME"),
+#         "USER": config("DB_USERNAME"),
+#         "PASSWORD": config("DB_PASSWORD", cast=str),
+#         "HOST": "db",
+#         "PORT": 5432,
+#     }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME"),
-        "USER": config("DB_USERNAME"),
-        "PASSWORD": config("DB_PASSWORD", cast=str),
-        "HOST": "db",
-        "PORT": 5432,
+    'default': {
+        dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 }
+
+
+
+# database_url = os.environ.get("DATABASE_URL")
+# DATABASES["default"] = dj_database_url.parse(database_url)
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from apps.account.views import dashboard
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('core/', include('apps.core.pages.urls')),
